cfa8fb395b82d2156546f2b922f7350e82eb6f1e
danielpauldiner <danielpauldiner@gmail.com>
2020-03-01 16:43:03 -0600

    Added area of circle function

--- task.py
+++ task.py
@@ -1,3 +1,13 @@
+import math
+
 
 def firstrun():
     return "success"
+
+def areaofcircle(radius):
+    if(type(radius) is not (int or float)):
+        return "Enter a float or int for the radius."
+    elif(radius <= 0):
+        return "Enter a positive number for the radius."
+    else:
+        return math.pi * radius * radius
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -12,6 +13,17 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def testradius1(self):
+        expected = "Enter a float or int for the radius."
+        self.assertEqual(expected, task.areaofcircle("test"))
+
+    def testradius2(self):
+        expected = "Enter a positive number for the radius."
+        self.assertEqual(expected, task.areaofcircle(0))
+
+    def testradius3(self):
+        expected = math.pi * 100
+        self.assertEqual(expected, task.areaofcircle(10))
 
 if __name__ == '__main__':
     unittest.main()
