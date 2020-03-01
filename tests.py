a2d0de2a1d0519b3fa9d15bc6b0af34b1e7b0f5c
danielpauldiner <danielpauldiner@gmail.com>
2020-03-01 16:08:52 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,3 +10,7 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+
+if __name__ == '__main__':
+    unittest.main()
